bf3fff2463555f7627c6a9b272ccec70924be0a9
David <nnjui13@gmail.com>
2026-01-16 12:18:10 +0300

    Added routes and CRUD

--- server/app.py
+++ server/app.py
@@ -19,9 +19,78 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
+
+
+class Heroes(Resource):
+    def get(self):
+        heroes = Hero.query.all()
+        return [hero.to_dict(rules=('-hero_powers',)) for hero in heroes], 200
+
+
+class HeroDetail(Resource):
+    def get(self, id):
+        hero = Hero.query.filter_by(id=id).first()
+        if not hero:
+            return {'error': 'Hero not found'}, 404
+        return hero.to_dict(), 200
+
+
+class Powers(Resource):
+    def get(self):
+        powers = Power.query.all()
+        return [power.to_dict(rules=('-hero_powers',)) for power in powers], 200
+
+
+class PowerDetail(Resource):
+    def get(self, id):
+        power = Power.query.filter_by(id=id).first()
+        if not power:
+            return {'error': 'Power not found'}, 404
+        return power.to_dict(rules=('-hero_powers',)), 200
+
+    def patch(self, id):
+        power = Power.query.filter_by(id=id).first()
+        if not power:
+            return {'error': 'Power not found'}, 404
+        
+        try:
+            data = request.get_json()
+            if 'description' in data:
+                power.description = data['description']
+            db.session.commit()
+            return power.to_dict(rules=('-hero_powers',)), 200
+        except ValueError:
+            db.session.rollback()
+            return {'errors': ['validation errors']}, 400
+
+
+class HeroPowers(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            hero_power = HeroPower(
+                hero_id=data['hero_id'],
+                power_id=data['power_id'],
+                strength=data['strength']
+            )
+            db.session.add(hero_power)
+            db.session.commit()
+            return hero_power.to_dict(), 200
+        except ValueError:
+            db.session.rollback()
+            return {'errors': ['validation errors']}, 400
+
+
+api.add_resource(Heroes, '/heroes')
+api.add_resource(HeroDetail, '/heroes/<int:id>')
+api.add_resource(Powers, '/powers')
+api.add_resource(PowerDetail, '/powers/<int:id>')
+api.add_resource(HeroPowers, '/hero_powers')
 
 
 if __name__ == '__main__':
